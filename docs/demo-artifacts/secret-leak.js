475b71507efe9dc74a7f940bb20b6348941609be
nnam099 <khachhanhtltd663@gmail.com>
2026-07-13 12:34:45 +0700

    Demo secret leak for Gitleaks

---
+++ docs/demo-artifacts/secret-leak.js
@@ -0,0 +1,5 @@
+// DEMO ONLY: intentionally fake credential used to prove Gitleaks blocks leaks.
+// Do not merge this branch into main.
+const leakedAwsAccessKeyId = 'AKIA1234567890ABCDEF';
+
+module.exports = { leakedAwsAccessKeyId };
